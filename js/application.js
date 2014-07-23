873ba649dc1da6ec37bcb2875757d6ca1535c11c
Timmy Huang <timmyhuang0404@gmail.com>
2014-07-22 23:16:03 -0700

    refactor code: remove if/else condition and replace with || operator

--- js/application.js
+++ js/application.js
@@ -6,17 +6,9 @@
   app.controller('ClickController', function(){
     this.clickInfo = {};
     this.sendCoords = function(event){
-
-      if ( event.offsetX == null ) {
-        this.clickInfo.mouseX = event.layerX;
-        this.clickInfo.mouseY = event.layerY;
-        this.clickInfo.time = video.currentTime();
-      } else {
-        this.clickInfo.mouseX = event.offsetX;
-        this.clickInfo.mouseY = event.offsetY;
-        this.clickInfo.time = video.currentTime();
-      }
-
+      this.clickInfo.mouseX = event.layerX || event.offsetX;
+      this.clickInfo.mouseY = event.layerY || event.offsetY;
+      this.clickInfo.time = video.currentTime();
       console.log(this.clickInfo);
     }; // sendCoords
   }); // ClickController
