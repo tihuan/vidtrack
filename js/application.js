ed8a24bea4df63fa5888c4cdf07f24fa27e131ca
Timmy Huang <timmyhuang0404@gmail.com>
2014-07-22 17:48:17 -0700

    Implement ng and bind click event on video to send coords

---
+++ js/application.js
@@ -0,0 +1,21 @@
+(function() {
+  var app = angular.module('track', []);
+  app.controller('ClickController', function(){
+    this.coords = [];
+    this.mouseX = 0;
+    this.mouseY = 0;
+    this.sendCoords = function(event){
+
+      if ( event.offsetX == null ) {
+        this.mouseX = event.originalEvent.layerX;
+        this.mouseY = event.originalEvent.layerY;
+      } else {
+        this.mouseX = event.offsetX;
+        this.mouseY = event.offsetY;
+      }
+      console.log(this.mouseX);
+      console.log("x: " + this.mouseX + ", " + "y: " + this.mouseY);
+    };
+  })
+
+}) ();
