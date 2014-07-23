a9172a30c5ba2d23ca95e59f64fd56c09a571cd5
Timmy Huang <timmyhuang0404@gmail.com>
2014-07-23 00:04:51 -0700

    change json_text to json_clickInfo for clarification

--- js/application.js
+++ js/application.js
@@ -1,21 +1,27 @@
 (function() {
   var video =  videojs("example_video_1");
-
   var app = angular.module('track', []);
 
   app.controller('ClickController', function(){
     this.clickInfo = {};
     this.sendCoords = function(event){
+      // event.layer for Firefox and event.offset for other browsers
       this.clickInfo.mouseX = event.layerX || event.offsetX;
       this.clickInfo.mouseY = event.layerY || event.offsetY;
       this.clickInfo.time = video.currentTime();
-      var json_text = angular.toJson(this.clickInfo)
-      console.log("Sending the following JSON string to server:");
-      console.log(json_text);
+
+      var json_clickInfo = angular.toJson(this.clickInfo)
+
+      console.log("Sending the following JSON data to server:");
+      console.log(json_clickInfo);
+
       $.ajax({
         url: "/gatherinfo",
         type: "POST",
-        data: json_text
+        data: json_clickInfo,
+        success: function() {
+          console.log("JSON was received by the server successfully.");
+        }
       });
     }; // sendCoords
   }); // ClickController
