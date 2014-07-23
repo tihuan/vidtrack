d0f5663ec5eb0c17112916d86b0b63e0eb6944ed
Timmy Huang <timmyhuang0404@gmail.com>
2014-07-23 00:39:49 -0700

    housekeeping

--- js/application.js
+++ js/application.js
@@ -1,28 +1,26 @@
 (function() {
   var video =  videojs("example_video_1");
   var app = angular.module('track', []);
-
   app.controller('ClickController', function(){
-    this.clickInfo = {};
     this.sendCoords = function(event){
+      this.clickInfo = {};
       // event.layer for Firefox and event.offset for other browsers
       this.clickInfo.mouseX = event.layerX || event.offsetX;
       this.clickInfo.mouseY = event.layerY || event.offsetY;
       this.clickInfo.time = video.currentTime();
-
-      var json_clickInfo = angular.toJson(this.clickInfo)
+      this.clickInfo.json = angular.toJson(this.clickInfo);
 
       console.log("Sending the following JSON data to server:");
-      console.log(json_clickInfo);
+      console.log(this.clickInfo.json);
 
       $.ajax({
         url: "/gatherinfo",
         type: "POST",
-        data: json_clickInfo,
+        data: this.clickInfo.json,
         success: function() {
           console.log("JSON was received by the server successfully.");
         }
-      });
+      }); // ajax
     }; // sendCoords
   }); // ClickController
 }) ();
